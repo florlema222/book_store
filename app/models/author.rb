127f1b2213fde08f3ef2f073e6a865160d137812
Florencia Lema <flor.lema222@gmail.com>
2023-10-27 16:53:44 -0300

    length limits added to validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,8 +2,18 @@ class Author < ApplicationRecord
   has_many :books
 
   validates :first_name, presence: true
+  validates :first_name, length: { maximum: 25 }
+
   validates :last_name, presence: true
+  validates :last_name, length: { maximum: 25 }
+
   validates :date_of_birth, presence: true
+  #no hay validador de fecha porque no me pareció necesario, viene formateado como fecha desde el formulario
+
   validates :about, presence: true
+  validates :about, length: { maximum: 1000 }
+
   validates :nationality, presence: true
+  validates :nationality, length: { maximum: 25 }
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,16 @@ class Book < ApplicationRecord
   belongs_to :publisher
 
   validates :title, presence: true
+  validates :title, length: { maximum: 255 }
+
   validates :isbn, uniqueness: true
+  validates :isbn, length: { maximum: 20 }
+
+  validates :review, presence: true
+  validates :review, length: { maximum: 1000 }
+
   validates :date_of_publication, presence: true
+  #no hay validador de fecha porque no me pareció necesario, viene formateado como fecha desde el formulario
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :author, presence: true
   validates :publisher, presence: true
